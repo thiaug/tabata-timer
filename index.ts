fe3ca68f92ed25d976321302fc77b05870203d2c
thiaug <60008101+thiaug@users.noreply.github.com>
2023-02-15 22:12:08 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
+//Tabata  é um treino de exercicios intervalados que consistem em 8 ciclos com 20 segundos de atividade e 10 segundos de descanso. Crie uma aplicação que ofereça ao usuário um temporizador com essas caracteristicas
 // Import stylesheets
 import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let counter: number = 20;
 
-let tabataTimer: HTMLElement = document.getElementById('timer');
-tabataTimer.innerHTML = `<h1>teste</h1>`;
+let tabataTimer = document.getElementById('title');
+tabataTimer.innerHTML = `<h1>Tabata Timer</h1>`;
+
+function startTimer() {
+  let intervalId = setInterval(() => {
+    counter = counter - 1;
+    console.log(counter);
+    document.getElementById('timer').innerHTML = `${counter}`;
+    if (counter === 0) clearInterval(intervalId);
+  }, 1000);
+}
+
+document.getElementById(`start-button`).addEventListener(`click`, () => {
+  startTimer();
+});
