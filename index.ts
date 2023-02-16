f1ec598bc57ddd83034604101f4d2cd91c829382
thiaug <60008101+thiaug@users.noreply.github.com>
2023-02-16 08:41:54 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 //Tabata  é um treino de exercicios intervalados que consistem em 8 ciclos com 20 segundos de atividade e 10 segundos de descanso. Crie uma aplicação que ofereça ao usuário um temporizador com essas caracteristicas
-// Import stylesheets
+
 import './style.css';
 
-let counter: number = 20;
+let counter: number = 21;
 
 let tabataTimer = document.getElementById('title');
 tabataTimer.innerHTML = `<h1>Tabata Timer</h1>`;
-
 function startTimer() {
   let intervalId = setInterval(() => {
     counter = counter - 1;
@@ -14,8 +13,17 @@ function startTimer() {
     document.getElementById('timer').innerHTML = `${counter}`;
     if (counter === 0) clearInterval(intervalId);
   }, 1000);
+  counter = 21;
+}
+
+function stopTimer() {
+  console.log(`chegou`);
 }
 
 document.getElementById(`start-button`).addEventListener(`click`, () => {
   startTimer();
+});
+
+document.getElementById(`stop-button`).addEventListener(`click`, () => {
+  stopTimer();
 });
